3766510bb1add3509442c45adf5d5c04d71e3e54
PLFLORCZUKT <Tomasz.Florczuk@tetrapak.com>
2025-06-03 13:57:35 +0200

    Added MongoDB connection

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
 import datetime
 from flask import Flask, render_template, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-entries = []
+# MONGODB_HOST = "localhost"
+# MONGODB_PORT = 27017
+
+client = MongoClient("mongodb://localhost:27017")
+app.db = client["Ideas"]
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    # print(client.list_database_names())
     if request.method == "POST":
         reqCont = request.form.get("content")
         date = datetime.datetime.today().strftime("%Y-%m-%d")
-        entries.append((reqCont, date))
+        app.db["Arbo"].insert_one({"content": reqCont,
+                                   "date": date,
+                                   })
 
     entriesWithDate = [
         (
-            entry[0],
-            entry[1],
-            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d")
+            entry["content"],
+            entry["date"],
+            datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
         )
-        for entry in entries
+        for entry in app.db["Arbo"].entries.find({})
     ]
 
     return render_template("home.html", entries=entriesWithDate)
