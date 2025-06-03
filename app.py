cf5ecd8f4e959e19804c6e9fce535a06e80a050f
PLFLORCZUKT <Tomasz.Florczuk@tetrapak.com>
2025-06-03 14:29:05 +0200

    Added app factory

--- app.py
+++ app.py
@@ -2,32 +2,34 @@ import datetime
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-# MONGODB_HOST = "localhost"
-# MONGODB_PORT = 27017
+    # MONGODB_HOST = "localhost"
+    # MONGODB_PORT = 27017
 
-client = MongoClient("mongodb://localhost:27017")
-app.db = client["Ideas"]
+    client = MongoClient("mongodb://localhost:27017")
+    app.db = client["Ideas"]
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    # print(client.list_database_names())
-    if request.method == "POST":
-        reqCont = request.form.get("content")
-        date = datetime.datetime.today().strftime("%Y-%m-%d")
-        app.db["Arbo"].insert_one({"content": reqCont,
-                                   "date": date,
-                                   })
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        # print(client.list_database_names())
+        if request.method == "POST":
+            reqCont = request.form.get("content")
+            date = datetime.datetime.today().strftime("%Y-%m-%d")
+            app.db["Arbo"].insert_one({"content": reqCont,
+                                       "date": date,
+                                       })
 
-    entriesWithDate = [
-        (
-            entry["content"],
-            entry["date"],
-            datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
-        )
-        for entry in app.db["Arbo"].find({})
-    ]
-    # print(entriesWithDate)
+        entriesWithDate = [
+            (
+                entry["content"],
+                entry["date"],
+                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+            )
+            for entry in app.db["Arbo"].find({})
+        ]
+        # print(entriesWithDate)
 
-    return render_template("home.html", entries=entriesWithDate)
+        return render_template("home.html", entries=entriesWithDate)
+    return app
